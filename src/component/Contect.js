7adaa23ac55bd31d3b0ffe6de5ae55f825dca822
aman <amanpatel22012003@gmail.com>
2023-08-28 13:37:24 +0530

    contact page

--- src/component/Contect.js
+++ src/component/Contect.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Contect = () => {
   return (
-    <div className="w-full  flex justify-between items-center px-[20%] lg:px-[15%] md:px-[25%] sm:px-[20%] xs:px-[5%]  py-[5%] bg-gray-100">
+    <div className="w-full  flex justify-between items-center px-[20%] lg:px-[15%] md:px-[25%] sm:px-[20%] xs:px-[3%]  py-[5%] bg-gray-100">
       <div className=" w-[45%] lg:w-[40%] md:hidden sm:hidden xs:hidden">
         <h1 className="font-bold text-4xl lg:text-3xl">Let's chat</h1>
         <h1 className="font-bold text-4xl lg:text-3xl">
@@ -12,32 +12,32 @@ export const Contect = () => {
           Let's create something togather
         </p>
       </div>
-      <div className="flex-col  w-[45%] lg:w-[50%] md:w-[100%] sm:w-[100%] xs:w-[100%] rounded-md  justify-center bg-white  py-[3%]">
-        <div className="w-[80%] text-center flex-col mx-[10%]">
-          <h1 className="text-2xl mb-[5%] font-semibold">CONTACT US</h1>
+      <div className="flex-col  w-[45%] lg:w-[50%] md:w-[100%]  sm:w-[100%] xs:w-[100%] rounded-md  justify-center bg-white  py-[3%]">
+        <div className="w-[80%] xs:w-[90%] text-center flex-col mx-[5%]">
+          <h1 className="text-2xl mb-[5%] xs:my-[6%] font-semibold">CONTACT US</h1>
 
-          <div className="w-full flex justify-center my-[3%]">
+          <div className="w-full flex justify-center my-[3%] xs:my-[5%]">
             <input
               type="text"
               className="  border border-gray-300 rounded-md w-full bg-gray-100 py-[1%] px-[2%] hover:border-gray-800 hover:font-semibold"
               placeholder="Enter your name"
             />
           </div>
-          <div className="w-full flex justify-center my-[3%]">
+          <div className="w-full flex justify-center my-[3%] xs:my-[5%]">
             <input
               type="email"
               className=" border border-gray-300  rounded-md w-full bg-gray-100 py-[1%] px-[2%] hover:border-gray-800 hover:font-semibold"
               placeholder="Email "
             />
           </div>
-          <div className=" w-full flex justify-center my-[3%]">
+          <div className=" w-full flex justify-center  my-[3%] xs:my-[5%]">
             <input
               type="password"
-              className="border border-gray-300 rounded-md w-full bg-gray-100 py-[1%] px-[2%] hover:border-gray-800 hover:font-semibold"
+              className="border border-gray-300 rounded-md w-full  bg-gray-100 py-[1%] px-[2%] hover:border-gray-800 hover:font-semibold"
               placeholder="Password"
             />
           </div>
-          <div className="w-full flex justify-center my-[3%]">
+          <div className="w-full flex justify-center my-[3%] xs:my-[5%]">
             <select
               name=""
               id=""
@@ -75,7 +75,7 @@ export const Contect = () => {
               </option>
             </select>
           </div>
-          <div className=" w-full flex justify-center   my-[3%] ">
+          <div className=" w-full flex justify-center   my-[3%] xs:my-[5%]">
             <textarea
               name=""
               placeholder="Enter your query here"
